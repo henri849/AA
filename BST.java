f13e6f7c339e2fd9f5be1f20be9a13b91995b127
henri849 <70386636+henri849@users.noreply.github.com>
2023-09-17 11:31:51 -0700

    Improved english, added printwalk, depth, successor, smallest, and improved other functions

--- BST.java
+++ BST.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class BST{
     private Node root;
     public BST(){};//default constructor could cause issues where functions don't anticipate the root being null
@@ -26,11 +28,27 @@ public class BST{
     }
 
     public int depth(){
-        return 1;//nub
+        return root.depth();
     }
 
     public String printwalk(){
-        return this.root.toString();//nub
+        //the idea here is to print the nodes in incremental order
+        Node current = this.getRoot();
+        
+        //next I consider using an assert but given that printwalking an empty tree is a valid use case I switched it out for an if
+        if (current == null) return "";//assert current != null : "Cannot printwalk empty tree";
+        
+        current = current.smallest();
+        Node next = current.successor();
+        String rtn = current.toString();
+        while (next != null){//we start with the smallest node and then print each successive successor.
+            if (next != null) rtn += " ";//adding spaces if there's a next element
+            rtn += next.toString();
+            current = next;
+            next = current.successor();
+            
+        }
+        return rtn;
     }
 
     public boolean isEmpty(){return root == null;};
@@ -41,12 +59,47 @@ class Tester{
     public Tester(){};//default constructor
     public void testNode(){
         NodeCheckInitialization(); 
+        NodeCheckSuccessor();
     }
     public void testBST(){
         BSTCheckInitialization(); //tests depth, isEmpty,getRoot and constructors
         BSTCheckInsert();
         BSTCheckRemove(); 
         BSTCheckPrintWalk(); 
+    }
+    private void NodeCheckSuccessor(){
+        BST tree = new BST();
+        //Standard uses for insert function:
+        tree.insert(1);
+        tree.insert(-1);
+        tree.insert(2);
+        tree.insert(1);
+        tree.insert(1);
+        tree.insert(2);
+        /*
+            1
+         -1   2
+           1 2
+          1
+        */
+       //testing smallest:
+       //right side of tree
+       if (tree.getRoot().child[1].smallest() == tree.getRoot().child[1].child[0])pass++;else fail++;
+       if (tree.getRoot().child[1].child[0].smallest() == tree.getRoot().child[1].child[0])pass++;else fail++;
+       //left side of tree
+       if (tree.getRoot().smallest() == tree.getRoot().child[0])pass++;else fail++;
+       if (tree.getRoot().child[0].smallest() == tree.getRoot().child[0])pass++;else fail++;
+       if (tree.getRoot().child[0].child[1].smallest() == tree.getRoot().child[0].child[1].child[0])pass++;else fail++;
+       if (tree.getRoot().child[0].child[1].child[0].smallest() == tree.getRoot().child[0].child[1].child[0])pass++;else fail++;
+       //testing successor:
+       //right side of tree
+       if (tree.getRoot().successor() == tree.getRoot().child[1].child[0])pass++;else fail++;
+       if (tree.getRoot().child[1].successor() == null)pass++;else fail++;
+       if (tree.getRoot().child[1].child[0].successor() == tree.getRoot().child[1])pass++;else fail++;
+       //left side of tree
+       if (tree.getRoot().child[0].successor() == tree.getRoot().child[0].child[1].child[0])pass++;else fail++;
+       if (tree.getRoot().child[0].child[1].successor() == tree.getRoot())pass++;else fail++;
+       if (tree.getRoot().child[0].child[1].child[0].successor() == tree.getRoot().child[0].child[1])pass++;else fail++;
     }
     private void NodeCheckInitialization(){
         Node n1 = new Node();//testing 1st constructor
@@ -73,10 +126,16 @@ class Tester{
         tree = new BST(new Node(1));//Same checks on alternate constructor
         if (tree.getRoot() != null)pass++; else fail++; 
         if (!tree.isEmpty())pass++; else fail++; 
+
+        //depth test
+        if (tree.depth() == 1)pass++; else fail++; 
+        for (int i =0; i < 100; i++){
+            tree.insert(ThreadLocalRandom.current().nextInt(-100,100));
+            if (tree.depth() == 2+i)pass++; else fail++; 
+        }
     }
     private void BSTCheckInsert(){
         BST tree = new BST();
-
         //Standard uses for insert function:
         tree.insert(1);
         if (tree.getRoot() != null)pass++; else fail++; //checks if a value of any type was assigned to the root
@@ -96,6 +155,12 @@ class Tester{
         if (tree.getRoot().child[0].child[1].child[0].key == 1 && tree.getRoot().child[0].child[1].child[0].child[0] == null && tree.getRoot().child[0].child[1].child[0].child[1] == null && tree.getRoot().child[0].child[1].child[0].parent == tree.getRoot().child[0].child[1])pass++; else fail++;
         tree.insert(2);
         if (tree.getRoot().child[1].child[0].key == 2 && tree.getRoot().child[1].child[0].child[0] == null && tree.getRoot().child[1].child[0].child[1] == null && tree.getRoot().child[1].child[0].parent == tree.getRoot().child[1])pass++; else fail++;
+        /*
+            1
+         -1   2
+           1 2
+          1
+        */
     }
 
     private void BSTCheckRemove(){
@@ -104,8 +169,32 @@ class Tester{
         if (tree.isEmpty())pass++; else fail++;
     }; 
     private void BSTCheckPrintWalk(){
-        BST tree = new BST(new Node(1));
-        if (tree.printwalk().equals("1"))pass++; else fail++;
+        BST tree = new BST();
+        if (tree.printwalk().equals(""))pass++; else fail++;//test empty tree (constructor 1)
+
+        tree = new BST(new Node(1));
+        if (tree.printwalk().equals("1"))pass++; else fail++;// test constructor 2's tree
+
+        String ans = "1";//test decent length but standard input w/ spacing
+        for (int i = 1; i < 100; i++){
+            tree.insert(i);
+            ans += " " + String.valueOf(i);
+            if (tree.printwalk().equals(ans))pass++; else fail++;
+        }
+
+        tree = new BST(new Node(1));//test standard use case with negatives and dupliactes
+        tree.insert(-1);
+        if (tree.printwalk().equals("-1 1"))pass++; else fail++;
+        tree.insert(2);
+        if (tree.printwalk().equals("-1 1 2"))pass++; else fail++;
+        tree.insert(1);
+        if (tree.printwalk().equals("-1 1 1 2"))pass++; else fail++;
+        tree.insert(1);
+        if (tree.printwalk().equals("-1 1 1 1 2"))pass++; else fail++;
+        tree.insert(2);
+        if (tree.printwalk().equals("-1 1 1 1 2 2"))pass++; else fail++;
+        tree.insert(-400);
+        if (tree.printwalk().equals("-400 -1 1 1 1 2 2"))pass++; else fail++;
     }; 
 }
 
@@ -118,11 +207,33 @@ class Node{
     public Node(int _key){
         key = _key;
     }
+    public int depth(){ //inclues the node it's called on
+        int total = 1;//recursion base case
+        if (this.child[0] != null){total+=this.child[0].depth();}// recursivly adds all leafs on the left branch
+        if (this.child[1] != null){total+=this.child[1].depth();}// recursivly adds all leafs on the right branch
+        return total;
+    }
     public Node(int _key, Node _parent){
         key = _key;
         parent = _parent;
     }
+    public Node successor(){
+        if (this.child[1] != null)return this.child[1].smallest(); // if there's a right child take the smallest of that branch
+        Node next = this.parent;
+        if (next == null) return null;//this is for the case where the only node is the root
+        Node current = this;
+        while (parent.child[1] == current){// go up the tree from the starting node until the branch becomes the left child of some other branch, that parent must be the successor assuming the tree is valid
+            current = parent;
+            parent = current.parent;
+            if (parent == null) return null;
+        }
+        return parent;
+    }
+    public Node smallest(){
+        if (this.child[0] == null) return this;//recusive minimum function, always looks for smallest node underneath by following left child
+        return this.child[0].smallest();
+    }
     public String toString(){
-        return String.valueOf(key);
+        return String.valueOf(key);//returns string version on int, should be changed to accomodate generics
     }
 }
