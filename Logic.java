5d65c7fb7a077b9e2dd170363744ab912af00e54
henri849 <70386636+henri849@users.noreply.github.com>
2024-01-18 21:56:11 -0800

    Add files via upload

--- Logic.java
+++ Logic.java
@@ -9,21 +9,25 @@ public class Logic{
         PC a = new PC(); 
         PC b = new PC(); 
         PC c = new PC(); 
+        PC d = new PC(); 
 
-        Logic o1 = new Imp(new And(new Imp(a,b),new Imp(b,c)),new Imp(a,c));
-        Logic o2 = new Imp(a,b);
+        Logic o1 = new Imp(new And(new Or(a,b),c),new Not(d));
+        // Logic o1 = new Imp(new And(new Imp(a,b),new Imp(b,c)),new Imp(a,c));
+        // Logic o2 = new Imp(a,b);
         // Logic o1 = new Imp(new Not(new Imp(new Or(a,b),c)),new And(a,c));      ¬((a ∨ b) ⊃ c) ⊃ (a ∧ c))
 
         System.out.println(o1); 
-        o1 = to_IND(o1);
-        System.out.println(o1); 
-        System.out.println(CNF(o1));
+        // o1 = to_IND(o1);
+        // System.out.println(o1); 
+        // System.out.println(CNF(o1));
+        System.out.println(CNF3(o1)); 
         //this currently works great, I just now need a system to simplify down the CNF
         // System.out.println(to_IND(o2)); 
     }
     public String toString(){
         return String.valueOf((char)id);
     }
+    public Logic newSelf(Logic l1, Logic l2){return new Logic();};
 
     public static void Tester(){
         int pass = 0; int fail =0;
@@ -60,8 +64,36 @@ public class Logic{
         if (im1.toString().equals("(A -> (B and C))"))pass++;else fail++;
         System.out.println("Operators, passed : "+pass + ", failed:" + fail + ".");
         pass = 0; fail =0;
+        Logic.count = 65;
+        //example from wikipedia
+        PC a2 = new PC(); 
+        PC b2 = new PC(); 
+        PC c2 = new PC(); 
+        PC d2 = new PC(); 
 
+        Logic o3 = new Imp(new And(new Or(a2,b2),c2),new Not(d2));
+        if (CNF3(o3).toString().equals("[I, (I <-> (J -> K)), (J <-> (N and O)), (N <-> (R or S)), (K <-> ~D)]"))pass++;else fail++;
+        Logic.count = 65;
         System.out.println("Forms, passed : "+pass + ", failed:" + fail + ".");
+    }
+
+    public static ArrayList<Logic> CNF3(Logic root){
+        ArrayList<Logic> rtn = new ArrayList<Logic>();
+        PC x1 = new PC();
+        rtn.add(x1);
+        CNF3R(root, rtn, x1);
+        return rtn;
+    }
+    public static void CNF3R(Logic root, ArrayList<Logic> rtn, PC thing){
+        if (root instanceof Not && root.i1 instanceof PC){
+            rtn.add(new Equiv(thing,root));
+        }else if (!(root instanceof PC)){
+            PC x1 = new PC();
+            PC x2 = new PC();
+            rtn.add(new Equiv(thing,root.newSelf(x1,x2)));
+            CNF3R(root.i1,rtn,x1);
+            CNF3R(root.i2,rtn,x2);
+        }
     }
 
     public static ArrayList<Logic> CNF(Logic root){
@@ -100,6 +132,11 @@ public class Logic{
             root.i1 = Logic.to_IND(root.i1);
             root.i2 = Logic.to_IND(root.i2);
         }
+        if (root instanceof Equiv){
+            root = new And(new Imp(root.i1,root.i2),new Imp(root.i2,root.i1));
+            root.i1 = Logic.to_IND(root.i1);
+            root.i2 = Logic.to_IND(root.i2);
+        }
         if (root instanceof Not && root.i1 instanceof Or){//demorgan's law
             root = new And(new Not(root.i1.i1), new Not(root.i1.i2));
             root.i1 = Logic.to_IND(root.i1);
@@ -126,6 +163,7 @@ public class Logic{
 }
 class PC extends Logic{
     public PC(){super();}
+    public PC newSelf(){return new PC();};
     public String toString(){
         return super.toString();
     }
@@ -136,6 +174,7 @@ class Not extends Logic{
         super();
         i1 = lg;
     }
+    public Not newSelf(Logic lg){return new Not(lg);};
     public String toString(){
         return "~" + i1;
     }
@@ -148,6 +187,7 @@ class Or extends Logic{
         i1 = l1;
         i2 = l2;
     }
+    public Or newSelf(Logic l1, Logic l2){return new Or(l1,l2);};
     public String toString(){
         return "(" + i1 + " or " + i2 + ")";
     }
@@ -159,6 +199,7 @@ class And extends Logic{
         i1 = l1;
         i2 = l2;
     }
+    public And newSelf(Logic l1, Logic l2){return new And(l1,l2);};
     public String toString(){
         return "(" + i1 + " and " + i2 + ")";
     }
@@ -171,8 +212,21 @@ class Imp extends Logic{
         i1 = l1;
         i2 = l2;    
     }
+    public Imp newSelf(Logic l1, Logic l2){return new Imp(l1,l2);};
     public String toString(){
         return "(" + i1 + " -> " + i2 + ")";
+    }
+    // public Boolean eval(Boolean s1, Boolean s2){return !i1.eval(s1) || i2.eval(s2);}
+}
+class Equiv extends Logic{
+    public Equiv(Logic l1, Logic l2){
+        super();
+        i1 = l1;
+        i2 = l2;    
+    }
+    public Equiv newSelf(Logic l1, Logic l2){return new Equiv(l1,l2);};
+    public String toString(){
+        return "(" + i1 + " <-> " + i2 + ")";
     }
     // public Boolean eval(Boolean s1, Boolean s2){return !i1.eval(s1) || i2.eval(s2);}
 }
